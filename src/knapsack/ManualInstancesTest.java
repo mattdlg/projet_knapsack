bd24639e69e08ca596138d95a9ec0162428fc603
Matt Dlg <matthieu.deleglise@insa-lyon.fr>
2026-01-06 15:32:39 +0100

    renaming files

--- src/knapsack/ManualInstancesTest.java
+++ src/knapsack/ManualInstancesTest.java
@@ -20,7 +20,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class Main
+public class ManualInstancesTest
 {
     public static void main( String[] args )
     {
